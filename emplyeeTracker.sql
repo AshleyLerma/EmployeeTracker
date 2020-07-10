160ee913987bcba54f3b04a71925ec69d8902173
Ashley Lerma <lerma.scout@gmail.com>
2020-07-10 14:40:41 -0500

    fixed syntax error

--- emplyeeTracker.sql
+++ emplyeeTracker.sql
@@ -19,6 +19,6 @@ CREATE TABLE employee(
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR (30) NOT NULL,
-  FOREIGN KEY (role_id) REFERENCES role(role_id)
-  FOREIGN KEY (manager_id) REFERENCES role(role_id)
+  FOREIGN KEY (role_id) REFERENCES role(role_id),
+  FOREIGN KEY (manager_id) REFERENCES role(role_id) NULL
 );

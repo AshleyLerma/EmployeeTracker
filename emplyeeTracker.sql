796430c90fc7bede1de7f388bb499c930e722435
Ashley Lerma <lerma.scout@gmail.com>
2020-07-10 14:39:14 -0500

    added foreign keys

--- emplyeeTracker.sql
+++ emplyeeTracker.sql
@@ -12,13 +12,13 @@ CREATE TABLE role(
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL (8,2) NOT NULL,
-  department_id INT NOT NULL
+  FOREIGN KEY (department_id) REFERENCES department(department_id)
 );
 
 CREATE TABLE employee(
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR (30) NOT NULL,
-  role_id INT NOT NULL,
-  manager_id INT NULL
+  FOREIGN KEY (role_id) REFERENCES role(role_id)
+  FOREIGN KEY (manager_id) REFERENCES role(role_id)
 );

80204cc7bd8fffa7969b71a5d3b52bd6c49c9a04
Ashley Lerma <lerma.scout@gmail.com>
2020-07-13 10:49:22 -0500

    updated SQL foreign keys

--- emplyeeTracker.sql
+++ emplyeeTracker.sql
@@ -4,21 +4,24 @@ CREATE DATABASE employee_DB;
 USE employee_DB;
 
 CREATE TABLE department(
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  department_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role(
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  role_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
-  salary DECIMAL (8,2) NOT NULL,
-  FOREIGN KEY (department_id) REFERENCES department(department_id)
+  salary DECIMAL (10,2) NOT NULL,
+  department_id INT NOT NULL,
+FOREIGN KEY (department_id) REFERENCES department(department_id)
 );
 
 CREATE TABLE employee(
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  employee_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR (30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT NULL, 
   FOREIGN KEY (role_id) REFERENCES role(role_id),
-  FOREIGN KEY (manager_id) REFERENCES role(role_id) NULL
+  FOREIGN KEY (manager_id) REFERENCES role(role_id)
 );

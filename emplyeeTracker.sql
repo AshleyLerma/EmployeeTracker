a98dfe7c4de5daae708ff66224d98be5e1060829
Ashley Lerma <lerma.scout@gmail.com>
2020-07-13 19:57:02 -0500

    cleaned up SQL

--- emplyeeTracker.sql
+++ emplyeeTracker.sql
@@ -10,21 +10,18 @@ CREATE TABLE department(
 
 CREATE TABLE role(
   role_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(30) NULL,
+  title VARCHAR(30),
   salary DECIMAL(10,2) NOT NULL DEFAULT 0.00,
-  department_id INT NULL,
-  FOREIGN KEY(department_id) REFERENCES department(department_id)  ON DELETE CASCADE
-  ON UPDATE CASCADE 
+  department_id INT,
+  FOREIGN KEY(department_id) REFERENCES department(department_id) ON DELETE CASCADE
 );
 
 CREATE TABLE employee(
   employee_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  role_id INT NULL,
-  manager_id INT NULL, 
-  FOREIGN KEY(role_id) REFERENCES role(role_id) ON DELETE CASCADE
-  ON UPDATE CASCADE,
+  role_id INT,
+  manager_id INT, 
+  FOREIGN KEY(role_id) REFERENCES role(role_id) ON DELETE CASCADE,
   FOREIGN KEY(manager_id) REFERENCES role(role_id) ON DELETE CASCADE
-  ON UPDATE CASCADE 
 );

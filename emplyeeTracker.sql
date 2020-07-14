d2048ef28e9833ecced97a4a53038f5640f2bd77
Ashley Lerma <lerma.scout@gmail.com>
2020-07-13 20:36:58 -0500

    cleaned up salary in sql

--- emplyeeTracker.sql
+++ emplyeeTracker.sql
@@ -11,7 +11,7 @@ CREATE TABLE department(
 CREATE TABLE role(
   role_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30),
-  salary DECIMAL(10,2) NOT NULL DEFAULT 0.00,
+  salary DECIMAL,
   department_id INT,
   FOREIGN KEY(department_id) REFERENCES department(department_id) ON DELETE CASCADE
 );
